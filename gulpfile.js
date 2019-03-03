a2e0af248f0006965000f126573b2103d6edb620
Aziz Ramos <sonicthehamilton.ar@gmail.com>
2019-03-03 00:19:09 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+
+var browserSync = require("browser-sync").create();
+
+// Put this after including our dependencies
+var paths = {
+    styles: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "styles/**/*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "styles"
+    }
+ 
+    // Easily add additional paths
+    // ,html: {
+    //  src: '...',
+    //  dest: '...'
+    // }
+};
+
+// Define tasks after requiring dependencies
+function style() {
+    // Where should gulp look for the sass files?
+    // My .sass files are stored in the styles folder
+    // (If you want to use scss files, simply look for *.scss files instead)
+	return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+            // Add browsersync stream pipe after compilation
+            .pipe(browserSync.stream())
+    );
+}
+
+// function reload() {
+//     browserSync.reload();
+// }
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(paths.styles.src, style);
+    // We should tell gulp which files to watch to trigger the reload
+    // This can be html or whatever you're using to develop your website
+    // Note -- you can obviously add the path to the Paths object
+    gulp.watch("./*.html", browserSync.reload);
+}
+
+
+    
+// Don't forget to expose the task!
+exports.watch = watch;
+ 
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
